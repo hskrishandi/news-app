5c6f34a8a88aabc3d4c8cf471462395e2b37d993
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-19 17:08:28 +0700

    Add news activity as main activity

--- app/src/main/java/com/hskris/newsapp/presentation/NewsActivity.kt
+++ app/src/main/java/com/hskris/newsapp/presentation/NewsActivity.kt
@@ -1,12 +1,13 @@
-package com.hskris.newsapp
+package com.hskris.newsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hskris.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
