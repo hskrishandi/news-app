b45969b4c9f9f4fd8f7365c37c5544c650797e88
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-19 14:00:21 +0700

    Refactor parse function to news model

--- app/src/main/java/com/hskris/newsapp/domain/GetHeadlineUseCase.kt
+++ app/src/main/java/com/hskris/newsapp/domain/GetHeadlineUseCase.kt
@@ -2,10 +2,8 @@ package com.hskris.newsapp.domain
 
 import com.hskris.newsapp.data.repository.NewsRepository
 import com.hskris.newsapp.data.repository.remote.NewsRemoteRepository
-import com.hskris.newsapp.data.repository.remote.models.NewsResponse
 import com.hskris.newsapp.domain.models.News
 import io.reactivex.Single
-import java.util.*
 
 class GetHeadlineUseCase(private val repo: NewsRepository, private val scheduler: BaseSchedulerProvider) {
 
@@ -17,21 +15,8 @@ class GetHeadlineUseCase(private val repo: NewsRepository, private val scheduler
         val result = repo.fetchHeadlines(query)
 
         return result.subscribeOn(scheduler.io())
-            .map {newsResponse -> parseResponseToModel(newsResponse)}
+            .map {newsResponse -> News.parseResponseToModel(newsResponse)}
             .observeOn(scheduler.ui())
-    }
-
-    private fun parseResponseToModel(newsResponse: NewsResponse): List<News> {
-
-        val articles = newsResponse.articles
-        val newsArray = arrayListOf<News>()
-
-        for(a in articles){
-            val news = News(a.title, a.author, Date(a.publishedAt), a.description, a.content, a.urlToImage)
-            newsArray.add(news)
-        }
-
-        return newsArray
     }
 
 }
--- app/src/main/java/com/hskris/newsapp/domain/models/News.kt
+++ app/src/main/java/com/hskris/newsapp/domain/models/News.kt
@@ -1,12 +1,28 @@
 package com.hskris.newsapp.domain.models
 
+import com.hskris.newsapp.data.repository.remote.models.NewsResponse
 import java.util.Date
 
-data class News (
+class News (
     val title: String,
     val author: String,
     val date: Date,
     val description: String,
     val content: String,
     val imageUrl: String
-)
+) {
+    companion object {
+        fun parseResponseToModel(newsResponse: NewsResponse): List<News> {
+
+            val articles = newsResponse.articles
+            val newsArray = arrayListOf<News>()
+
+            for(a in articles){
+                val news = News(a.title, a.author, Date(a.publishedAt), a.description, a.content, a.urlToImage)
+                newsArray.add(news)
+            }
+
+            return newsArray
+        }
+    }
+}
