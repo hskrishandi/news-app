89ffa1e487eb5517d7e707b1cb63ce48e8d331d1
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-19 14:49:24 +0700

    Fix get headline use case test for returning correct news model single

--- app/src/test/java/com/hskris/newsapp/domain/GetHeadlineUseCaseTest.kt
+++ app/src/test/java/com/hskris/newsapp/domain/GetHeadlineUseCaseTest.kt
@@ -1,7 +1,10 @@
 package com.hskris.newsapp.domain
 
 import com.hskris.newsapp.data.repository.NewsRepository
+import com.hskris.newsapp.data.repository.remote.models.Articles
 import com.hskris.newsapp.data.repository.remote.models.NewsResponse
+import com.hskris.newsapp.data.repository.remote.models.Source
+import com.hskris.newsapp.domain.models.News
 import com.nhaarman.mockitokotlin2.*
 import io.reactivex.Single
 import org.jetbrains.spek.api.Spek
@@ -9,6 +12,7 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 
 class GetHeadlineUseCaseTest : Spek({
     given("get headline use case") {
@@ -17,8 +21,14 @@ class GetHeadlineUseCaseTest : Spek({
         val usecase = GetHeadlineUseCase(repo, scheduler)
 
         on("getting by category") {
-            val newsResponse = NewsResponse("", 0, emptyList())
+
+            val articles = listOf(
+                Articles(Source("1", ""), "hans", "", "", "", "", "2019-06-20T23:07:41Z", ""),
+                Articles(Source("2", ""), "rico", "", "", "", "", "2019-06-20T23:07:41Z", ""))
+
+            val newsResponse = NewsResponse("", 0, articles)
             val newsSingle = Single.just(newsResponse)
+
             doReturn(newsSingle).`when`(repo).fetchHeadlines(any())
 
             val result = usecase.getByCategory("some category")
@@ -28,9 +38,11 @@ class GetHeadlineUseCaseTest : Spek({
             }
 
             it("should return correct news model single") {
-                result.subscribe{ news ->
-                    assertEquals(newsResponse.articles.size, news.size)
-                }
+                result.test()
+                    .assertNoErrors()
+                    .assertValue {
+                        it.size == 2 && it[0].author == "hans"
+                    }
             }
         }
     }
