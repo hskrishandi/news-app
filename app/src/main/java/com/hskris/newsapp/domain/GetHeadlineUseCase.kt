f683c3d3b402b46170bd471bdc3b15b17edaefd9
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-19 15:29:48 +0700

    Add default country for get headline use case

--- app/src/main/java/com/hskris/newsapp/domain/GetHeadlineUseCase.kt
+++ app/src/main/java/com/hskris/newsapp/domain/GetHeadlineUseCase.kt
@@ -5,11 +5,15 @@ import com.hskris.newsapp.data.repository.remote.NewsRemoteRepository
 import com.hskris.newsapp.domain.models.News
 import io.reactivex.Single
 
-class GetHeadlineUseCase(private val repo: NewsRepository, private val scheduler: BaseSchedulerProvider) {
-
+class GetHeadlineUseCase(
+    private val repo: NewsRepository,
+    private val scheduler: BaseSchedulerProvider,
+    val country: String = "us"
+) {
     fun getByCategory(category: String): Single<List<News>> {
 
         val query = NewsRemoteRepository.NewsRemoteQuery()
+        query.add("country", country)
         query.add("category", category)
 
         val result = repo.fetchHeadlines(query)
